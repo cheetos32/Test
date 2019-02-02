fde2a514b8a8fe8b3fb9b3ac86218f50cb476f27
Shcherbakov Alexandr <34583499+cheetos32@users.noreply.github.com>
2019-02-02 11:06:48 +0300

    Bottom Navigation
    Сделал нижнию навигацию для двух активити

--- app/src/main/java/com/hfad/test/MainActivity.java
+++ app/src/main/java/com/hfad/test/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hfad.test;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -14,9 +17,47 @@ public class MainActivity extends AppCompatActivity {
     public Button vivodButton;
     public Button buttonTest;
     public TextView rezultat;
+    private TextView mTextMessage;
     public Spinner cvetPiva;
     public Spinner strana;
     public Spinner krepost;
+
+
+
+
+
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +69,26 @@ public class MainActivity extends AppCompatActivity {
         this.cvetPiva = (Spinner) findViewById(R.id.spinner);
         this.strana = (Spinner) findViewById(R.id.spinner2);
         this.krepost = (Spinner) findViewById(R.id.spinner3);
+
+
+
+
+
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+
+
+
+
+
+
+
     }
     public void onClick2 (View view) {
         Intent intent = new Intent(this, ActivityList.class);
